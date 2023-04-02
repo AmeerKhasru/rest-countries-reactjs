6b1fa21fbc23c4bf998f9ccc8bf87b3c17f1288e
Amir Khasru <muhammadamirkhasru@gmail.com>
2023-04-03 05:25:58 +0600

    Props added

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { useEffect, useState } from 'react';
 function App() {
   
   return (
-    <LoadCountries></LoadCountries>
+    
+      <LoadCountries></LoadCountries>
+   
   );
 }
 function LoadCountries(){
@@ -18,10 +20,22 @@ function LoadCountries(){
   },[])
   return(
     <div>
-      <h1>Visiting Every Contry of the World</h1>
+      <h1>Visiting Every Country of the World</h1>
       <h3>Available Countries: {countries.length}</h3>
+      {
+        countries.map(country => <Country name ={country.name.common}></Country>)
+      }
     </div>
+    
   )
 }
 
+function Country(props){
+  return(
+    <div>
+      <h2>Name: {props.name}</h2>
+      
+    </div>
+  )
+}
 export default App;
